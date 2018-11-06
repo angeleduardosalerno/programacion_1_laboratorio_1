02f860e79a7169cbe5eb7430750a52d3c655eba8
Angel <40776521+angeleduardosalerno@users.noreply.github.com>
2018-11-05 22:11:05 -0300

    TP 3 CON ANOTACIONES PARA HACER!!!

---
+++ Clase_21/TP3 ----CON ANOTACIONES EN CLASE/Win_32/Controller.c
@@ -0,0 +1,130 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "LinkedList.h"
+#include "Employee.h"
+
+
+/** \brief Carga los datos de los empleados desde el archivo data.csv (modo texto).
+ *
+ * \param path char*
+ * \param pArrayListEmployee LinkedList*
+ * \return int
+ *
+ */
+int controller_loadFromText(char* fileName , LinkedList* pArrayListEmployee) // CONTROLLER CONTROLA SI ESTA OK!!1
+{
+    //ACA HAY QUE PROGRAMAR EL PARSEO....ACA HAY QUE LLAMAR AL PARSE, LE PASAS AL ARCHIVO
+    FILE *pFile;
+
+    // HAY QUE CREAR LAS VARIABLES EMPLOYEE CON RESERVA DINAMICA DE MEMORIA SINO NO SE PUEDE GUARDAR EN NINGUN LADO ESTO...
+    pFile = fopen(fileName,"r"); // OJOOOOOO ACA VA EL TIPO LO Q TENGO QUE HACER! ES MEJOR ABRIRLO POR CADA ITEM DE ACUERDO A LO Q TE PIDAN!!
+    // O SEA SI SE PUDO ABRIR EL ARCHIVO...! SE HACE EL PARSEO!!
+    if(pFile != NULL && fileName != NULL) // ACA HAY QUE PREGUNTAR POR LOS DOS, HAY QUE PREGUNTAR POR EL ERROR...
+    {
+        parser_EmployeeFromText(pFile , pArrayListEmployee);
+    }
+    else
+    {
+        return -1;
+        fclose(pFile);
+    }
+
+
+}
+
+/** \brief Carga los datos de los empleados desde el archivo data.csv (modo binario).
+ *
+ * \param path char*
+ * \param pArrayListEmployee LinkedList*
+ * \return int
+ *
+ */
+int controller_loadFromBinary(char* path , LinkedList* pArrayListEmployee)
+{
+    return 1;
+}
+
+/** \brief Alta de empleados
+ *
+ * \param path char*
+ * \param pArrayListEmployee LinkedList*
+ * \return int
+ *
+ */
+int controller_addEmployee(LinkedList* pArrayListEmployee)
+{
+    return 1;
+}
+
+/** \brief Modificar datos de empleado
+ *
+ * \param path char*
+ * \param pArrayListEmployee LinkedList*
+ * \return int
+ *
+ */
+int controller_editEmployee(LinkedList* pArrayListEmployee)
+{
+    return 1;
+}
+
+/** \brief Baja de empleado
+ *
+ * \param path char*
+ * \param pArrayListEmployee LinkedList*
+ * \return int
+ *
+ */
+int controller_removeEmployee(LinkedList* pArrayListEmployee)
+{
+    return 1;
+}
+
+/** \brief Listar empleados
+ *
+ * \param path char*
+ * \param pArrayListEmployee LinkedList*
+ * \return int
+ *
+ */
+int controller_ListEmployee(LinkedList* pArrayListEmployee)
+{
+    return 1;
+}
+
+/** \brief Ordenar empleados
+ *
+ * \param path char*
+ * \param pArrayListEmployee LinkedList*
+ * \return int
+ *
+ */
+int controller_sortEmployee(LinkedList* pArrayListEmployee)
+{
+    return 1;
+}
+
+/** \brief Guarda los datos de los empleados en el archivo data.csv (modo texto).
+ *
+ * \param path char*
+ * \param pArrayListEmployee LinkedList*
+ * \return int
+ *
+ */
+int controller_saveAsText(char* path , LinkedList* pArrayListEmployee)
+{
+    return 1;
+}
+
+/** \brief Guarda los datos de los empleados en el archivo data.csv (modo binario).
+ *
+ * \param path char*
+ * \param pArrayListEmployee LinkedList*
+ * \return int
+ *
+ */
+int controller_saveAsBinary(char* path , LinkedList* pArrayListEmployee)
+{
+    return 1;
+}
+
---
+++ Clase_21/TP3 ----CON ANOTACIONES EN CLASE/Win_32/parser.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "LinkedList.h"
+#include "Employee.h"
+
+/** \brief Parsea los datos los datos de los empleados desde el archivo data.csv (modo texto).
+ *
+ * \param path char*
+ * \param pArrayListEmployee LinkedList*
+ * \return int
+ *
+ */
+int parser_EmployeeFromText(FILE* pFile , LinkedList* pArrayListEmployee)
+{
+    int r,i=0;
+    char var1[50], var2[50] ,var3[50] ,var4[50]; // VARIABLES AUX EN Q SE ALMACENA LOS DATOS DEL ARCHIVO LEIDOS COMO STRING
+
+
+    r = fscanf(pFile,"%[^,],%[^,],%[^,],%[^\n]\n",var1,var2,var3,var4);// LECTURA EN FALSO PARA EVITAR LOS TITULOS
+
+    do{
+
+        r = fscanf(pFile,"%[^,],%[^,],%[^,],%[^\n]\n",var1,var2,var3,var4); // SE LEE DEL ARCHIVO LOS ELEMENTOS COMO STRING...
+
+        if(r==4) // SI PUDO LEER LOS 4 ELEMENTOS DEL ARCHIVO: EL id,nombre,horasTrabajadas,sueldo
+        {
+            Employee* e = employee_newEmployee();
+            employee_initialize(&e[i], var1, var2, var3, var4); // ACA HAY QUE PASAR LOS STRING A SUS RESPECTIVOS FORMATOS Y GUARDALO EN LA LISTA
+            i++;
+            ll_add(pArrayListEmployee,e); // SE CARGAN LOS DATOS GUARDADOS EN LA ESTRUCTURA EN LA LISTA!
+            //ACA HAY QUE USAR EL LL ADD DE LA LISTA!!!
+        }
+        else
+        {
+            break;
+        }
+
+    }while(!feof(pFile));
+
+    fclose(pFile);
+
+    return 1;
+}
+
+/** \brief Parsea los datos los datos de los empleados desde el archivo data.csv (modo binario).
+ *
+ * \param path char*
+ * \param pArrayListEmployee LinkedList*
+ * \return int
+ *
+ */
+int parser_EmployeeFromBinary(FILE* pFile , LinkedList* pArrayListEmployee)
+{
+
+    return 1;
+}
